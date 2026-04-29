19bc56877c76cb379ec1da5df7c25e71fe1591ae
Aditya <adityaguptavns1@gmail.com>
2026-04-29 14:48:45 +0530

    add eventcard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,14 +5,18 @@ import Link from "next/link"
 const Navbar = () => {
     return (
         <header>
-            <nav>
+            <nav className="flex justify-between">
+                <div>
                 <Link href='/' className="logo">
                     <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
                     <p>DevExpo</p>
                 </Link>
+                </div>
+                <div className="flex gap-6">
                 <Link href='/'>Home</Link>
                 <Link href='/'>Events</Link>
                 <Link href='/'>Create Event</Link>
+                </div>
             </nav>
         </header>
     )
