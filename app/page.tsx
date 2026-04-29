d668e03207a9b882bcbc59e2d885bcafe68efde1
Aditya <adityaguptavns1@gmail.com>
2026-04-29 14:52:01 +0530

    add cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,29 @@
+import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
-import React from 'react'
 
+const events = [
+  { image: '/images/event1.png', title: 'Event 1' },
+  { image: '/images/event2.png', title: 'Event 2' },
+]
 const page = () => {
   return (
     <section>
       <h1 className='text-center'>The Hub for Every Dev <br /> Event you Cant Miss</h1>
       <p className='text-center mt-5'>Hackathons, Meetups and Conferences All in One Place</p>
-      <ExploreBtn/>
+      <ExploreBtn />
 
       <div className='mt-20 space-y-7'>
         <h3>Featured Events</h3>
 
 
 
-        <ul className='events'> 
-          {[1,2,3,4,5].map((event) => (
-            <li key={event}> Event {event}
-
+        <ul className='events'>
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
             </li>
           ))}
-          </ul>
+        </ul>
       </div>
     </section>
   )
