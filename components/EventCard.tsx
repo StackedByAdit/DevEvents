384ffa893c9e9327dad9eb91f1f77adb5baf607c
Aditya <adityaguptavns1@gmail.com>
2026-04-29 23:58:30 +0530

    add event interface

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -2,19 +2,26 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
-title : string;
-image : string;
+    id: string;
+    title: string;
+    date: string;
+    location: string;
+    category: string;
+    attendees: number;
+    price: string;
+    image: string;
+    description: string;
 }
 
-const EventCard = ({title, image} : Props) => {
+const EventCard = ({ title, image }: Props) => {
 
-  return (
-        
-<Link href={'/events'} id="event-card">
-    <Image src={image} alt={title} width={410} height={300} className="poster" />
-    <p className="title">{title}</p>
-</Link>
-  )
+    return (
+
+        <Link href={'/events'} id="event-card">
+            <Image src={image} alt={title} width={410} height={300} className="poster" />
+            <p className="title">{title}</p>
+        </Link>
+    )
 }
 
 export default EventCard
