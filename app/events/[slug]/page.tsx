dd121be582296c83c5121dff93c2393bd665350c
Aditya <adityaguptavns1@gmail.com>
2026-04-30 23:42:22 +0530

    add event details and agenda

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -3,16 +3,24 @@ import { notFound } from "next/navigation";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const EventDetailItem = ({ icon, alt, label } : {icon : string; alt : string; label : string; }) => (
+const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; label: string; }) => (
     <div className="flex-row-gap-2 items-center">
         <Image src={icon} alt={alt} width={17} height={17} />
         <p>{label}</p>
     </div>
 )
+
+const EventAgenda = ({ agendaItems } : { agendaItems :  string[]}) => (
+    <div className="agenda">
+        <h2>Agenda</h2>
+        <ul>
+            {agendaItems.map((item) => (
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+    </div>
+)
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
-
-
-
 
     const { slug } = await params;
 
@@ -31,7 +39,7 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
             <div className="details">
                 {/* Left side - event content */}
                 <div className="content">
-                    <Image src={image} alt="Event Banner" width={800} height={800} className="banner"/>
+                    <Image src={image} alt="Event Banner" width={800} height={800} className="banner" />
 
                     <section className="flex-col-gap-2">
                         <h2>Overview</h2>
@@ -43,12 +51,19 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
                         <h2>Event Details</h2>
 
                         <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
+                        <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
+                        <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
+                        <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
+                        <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
+
                     </section>
+
+                    <EventAgenda agendaItems={JSON.parse(agenda[0])} />
                 </div>
                 {/* Right side - booking form*/}
-                    <aside className="booking">
-                        <p className="text-lg-font-semibold">Book Event</p>
-                    </aside>
+                <aside className="booking">
+                    <p className="text-lg-font-semibold">Book Event</p>
+                </aside>
             </div>
         </section>
     )
