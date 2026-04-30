135938bcd13e8bcf69e13abc41ddd2108712f9d5
Aditya <adityaguptavns1@gmail.com>
2026-05-01 01:36:31 +0530

    add submit handler

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -5,12 +5,22 @@ import { useState } from "react"
 const BookEvent = () => {
     const [email, setEmail] = useState("");
     const [submitted, setSubmitted] = useState(false)
+
+    const handleSubmit = (e : React.FormEvent) => {
+        e.preventDefault();
+
+
+        setTimeout(() => {
+            setSubmitted(true);
+        }, 1000)
+    }
+
     return (
         <div id='book-event'>
             {submitted ? (
                 <p className="text-sm">Thank you for signing up!</p>
             ) : (
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="email">Email Address</label>
                         <input type="email"
