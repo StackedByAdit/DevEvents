8ea7324dc28ba6caa10d4027b03e2793ddb4fb73
Aditya <adityaguptavns1@gmail.com>
2026-04-30 07:42:30 +0530

    add get function for event

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -16,15 +16,15 @@ export async function POST(req: NextRequest) {
         } catch (e) {
             return NextResponse.json({
                 message: 'Invalid JSON data format'
-            }, { status: 400   })
+            }, { status: 400 })
         }
 
         const file = formData.get('image') as File;
 
-        if(!file){
+        if (!file) {
             return NextResponse.json({
-                message : 'Image file is required'
-            }, { status : 400})
+                message: 'Image file is required'
+            }, { status: 400 })
         }
 
         const arrayBuffer = await file.arrayBuffer();
@@ -32,17 +32,17 @@ export async function POST(req: NextRequest) {
         const buffer = Buffer.from(arrayBuffer);
 
         const uploadResult = await new Promise((resolve, reject) => {
-             cloudinary.uploader.upload_stream({resource_type: 'image', folder: "devEvent" }, (error, results) => {
-                if(error) return reject(error);
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: "devEvent" }, (error, results) => {
+                if (error) return reject(error);
                 resolve(results);
-             }).end(buffer)
+            }).end(buffer)
         })
 
-        event.image = (uploadResult as { secure_url : string}).secure_url
+        event.image = (uploadResult as { secure_url: string }).secure_url
 
         const createEvent = await Event.create(event);
 
-        return NextResponse.json({message : 'Event created successfully', event : createEvent}, { status : 201 })
+        return NextResponse.json({ message: 'Event created successfully', event: createEvent }, { status: 201 })
 
 
     } catch (e) {
@@ -55,5 +55,22 @@ export async function POST(req: NextRequest) {
             },
             { status: 500 }
         );
+    }
+}
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        const events = await Event.find().sort({ createdAt: -1 })
+
+        return NextResponse.json({
+            message: "Event fetched successfully", events
+        },
+            { status: 200 })
+    } catch (e) {
+        return NextResponse.json({ message: 'Event fetching failed', error: e },
+            { status: 500 }
+        )
     }
 }
