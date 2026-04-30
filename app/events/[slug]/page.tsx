811f7eaa86c7fa764650183c13db06fa0d61c63b
Aditya <adityaguptavns1@gmail.com>
2026-05-01 01:33:08 +0530

    add Book Event feature

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import BookEvent from "@/components/BookEvent";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -41,6 +42,8 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
 
     if (!description) return notFound();
 
+    const bookings = 10;
+
     return (
         <section id="event">
             <div className="header">
@@ -81,7 +84,18 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
                 </div>
                 {/* Right side - booking form*/}
                 <aside className="booking">
-                    <p className="text-lg-font-semibold">Book Event</p>
+                    <div className="signup-card">
+                        <h2>Book Your Spot</h2>
+                        {bookings > 10 ? (
+                            <p className="text-sm">
+                                Join { bookings } people who have already booked their spot!
+                            </p>
+                        ) : (
+                            <p className="text-sm">Be the first to book your spot!</p>
+                        )}
+
+                        <BookEvent/>
+                    </div>
                 </aside>
             </div>
         </section>
